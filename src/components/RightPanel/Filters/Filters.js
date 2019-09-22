2c4c730be060b2aa91bd51e835e8e65a446d10dd
Shubham Mishra <shubhammshr621@gmail.com>
2019-09-22 21:02:50 +0530

    pushing default state to filters

--- src/components/RightPanel/Filters/Filters.js
+++ src/components/RightPanel/Filters/Filters.js
@@ -28,11 +28,19 @@ function Filters(props) {
 		</select>
 		<label>Year</label>
 		<select>
-			<option>ABC</option>
+			<option>2019</option>
+			<option>2018</option>
+			<option>2017</option>
+			<option>2016</option>
+			<option>2015</option>
 		</select>
 		<label>Rating</label>
 		<select>
-			<option>ABC</option>
+			<option>1</option>
+			<option>2</option>
+			<option>3</option>
+			<option>4</option>
+			<option>5</option>
 		</select>
 	</div>
 }
